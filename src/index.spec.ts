2c642d4bcf10d4bfbfa4d4edd46799e6ae24c673
Andre Torgal <andre.torgal@gmail.com>
2019-02-09 16:22:02 +0100

    chore: test runner boilerplate

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,12 +3,12 @@ import 'mocha';
 import 'sinon';
 import sinonChai from 'sinon-chai';
 
-import { Timing } from './index';
+import { Timing } from '.';
 
 const expect = chai.expect;
 chai.use(sinonChai);
 
-describe('class-timing', function t () {
+describe('package: class-timing', function t () {
   it('should export the expected symbols', function t () {
     expect(Timing).to.be.a('function');
   });
